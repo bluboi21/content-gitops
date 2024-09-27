82738772612bc4a0a9c219582988ae8147fb01ab
Blu Boi <125328216+bluboi21@users.noreply.github.com>
2024-09-27 00:53:45 -0700

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Whos World Is This???"
+    return "Whos World Is This?"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
